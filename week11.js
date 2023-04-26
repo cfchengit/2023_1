4109cc30fcc36b782682bc12cc928ba499e13ab1
cfchen_tku <cfchengit@users.noreply.github.com>
2023-04-26 12:00:27 +0800

    Add files via upload

---
+++ week11.js
@@ -0,0 +1,81 @@
+var vm = new Vue({
+    el: "#app",
+    data: {
+      keyword: "",
+      cards: [
+        {
+          link:"https://h5p.org/h5p/embed/107824",
+          title: "排序看" ,
+          width: "100%" ,
+          height: "100%" 
+          // width: "1090px" ,
+          // height: "760px"         
+        },
+        {
+          // link:"https://h5p.org/h5p/embed/554805",
+          link:"https://www.tku.edu.tw",
+            title: "找看看",
+            width: "100%" ,
+          height: "100%" 
+            // width: "1090px" ,
+            // height: "815px"
+
+          },
+          {
+            link:"https://h5p.org/h5p/embed/231678",
+            title: "配對",
+            width: "1090px" ,
+            height: "474px"
+            
+          },
+          {
+            link:"https://h5p.org/h5p/embed/707",
+            title: "記憶",
+            width: "1090px" ,
+            height: "232px"
+          },
+          {
+            link:"https://www.youtube.com/embed/BxwK92i93OE",
+            title: "音樂",
+            width: "100%" ,
+            height: "100%"
+            // width: "560px" ,
+            // height: "315px"
+          },
+          {
+            link:"https://h5p.org/h5p/embed/617",
+            title: "食譜教學",
+            width: "100%" ,
+            height: "100%"
+            // width: "560px" ,
+            // height: "315px"
+          },
+          {
+            link:"https://h5p.org/h5p/embed/440740",
+            title: "藝術欣賞",
+            width: "100%" ,
+            height: "100%"
+            // width: "560px" ,
+            // height: "315px"
+          }
+          
+          
+
+          
+		],
+    },
+    
+    mounted:function(){        
+        swiper = new Swiper('.carousel', {            
+            loop: true, 
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+            pagination: {
+                el: '.swiper-pagination'
+            }
+        })     
+
+        }
+  });
